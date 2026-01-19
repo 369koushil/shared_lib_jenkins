b7d494748ba056a1637ef55d4e9c49973739d613
369koushil <koushil463@gmail.com>
2026-01-19 23:39:32 +0530

    changed file name

--- vars/dockerComposeUp.groovy
+++
@@ -1,4 +0,0 @@
-def dockerCompose(){
-    ech "exec docker compose up cmd"
-    sh "docker compose up --build"
-}
--- vars/dockerPull.groovy
+++
@@ -1,5 +0,0 @@
-def pullingDockerImages(String image,String version="latest"){
-    echo "starting pulling images"
-    sh "docker pull ${image}:${version}"
-    echo "completed pulling images"
-}
