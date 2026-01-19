197b48357b2a4ef8db3ba052d0e41dc52a29d904
369koushil <koushil463@gmail.com>
2026-01-20 00:36:21 +0530

    modified buildfunc

--- vars/buildDockerImages.groovy
+++ vars/buildDockerImages.groovy
@@ -1,5 +1,5 @@
-def call(String DOCKER_USERNAME,String imgName){
+def call(String DOCKER_USERNAME,String imgName,String context="."){
     echo "starting building images"
-    sh "docker build -t ${DOCKER_USERNAME}/${imgName} ."
+    sh "docker build -t ${DOCKER_USERNAME}/${imgName} ./${context}"
     echo "completed building images"
 }
